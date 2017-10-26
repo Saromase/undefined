e03833fb5f1749dea9bd55dffd92fc71ae452104
Romain <seite.romain@yahoo.com>
2017-10-26 11:39:32 +0200

    Update Fixture for Variation

--- src/AppBundle/DataFixtures/ORM/Fixtures.php
+++ src/AppBundle/DataFixtures/ORM/Fixtures.php
@@ -11,13 +11,28 @@ class Fixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $name = ['Eau', 'Pierre', 'Gaz', 'Petrole', 'Aluminium', 'Or', 'Cuivre', 'Fer', 'Sable', 'Charbon', 'Bois'];
+        $temp = 0;
+        $final = [];
+
         for ($i = 0; $i < count($name); $i++) {
+            $quantity = mt_rand(1000,15000);
+            $temp += $quantity;
+            array_push($final, [
+                'name' => $name[$i],
+                'quantity' => $quantity,
+                'variation' => rand(-50 , 50)
+            ]);
+        }
+
+        foreach ($final as $datas) {
+            $price = $temp / $datas['quantity'] * 25;
             $product = new Product();
-            $product->setName($name[$i]);
-            $product->setQuantity(mt_rand(1000,15000));
-            $product->setMidPrice(mt_rand(1, 500));
-            $product->setPrice(mt_rand(10, 500));
-            $product->setTier(1);
+            $product->setName($datas['name'])
+                ->setQuantity($datas['quantity'])
+                ->setMidPrice($price)
+                ->setPrice($price)
+                ->setTier(1)
+                ->setVariation($datas['variation']);
             $manager->persist($product);
         }
 

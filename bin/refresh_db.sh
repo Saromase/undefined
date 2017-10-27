750d60fdcd41c4e2739791e26ddd47ab58799789
François MATHIEU <francois.mathieu@livexp.fr>
2017-10-27 15:24:49 +0200

    Update import script

--- bin/refresh_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-php bin/console doctrine:database:drop --force
-php bin/console doctrine:database:create
-php bin/console doctrine:schema:update --force
-php bin/console doctrine:fixtures:load
-php bin/console fos:user:create admin admin@admin.fr discord --super-admin
